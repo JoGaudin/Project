d9bb56004d665e9d793e8cbf420a5829e8f341a7
JoGaudin <johan.gaudin.nlc@gmail.com>
2022-11-24 08:25:45 +0100

    activity_main.xml

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -6,17 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button toProfileBtn;
+    private ImageButton toProfileImgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toProfileImgBtn = (ImageButton) findViewById(R.id.goToProfileImageButton);
         toProfileBtn = (Button) findViewById(R.id.goToProfileButton);
+
+
+        toProfileImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openProfileActivity();}
+        });
         toProfileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
