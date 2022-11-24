c4a63deda89edf7f4a091c0f2df8dfd97fa85584
AtinyStay <72931462+AtinyStay@users.noreply.github.com>
2022-11-24 14:27:09 +0000

    cleaning unused files + profile improvement (almost done!)

--- app/src/main/java/com/example/project/Profile_v2.java
+++ app/src/main/java/com/example/project/Profile_v2.java
@@ -3,6 +3,7 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,14 +15,19 @@ import android.widget.Toast;
 
 public class Profile_v2 extends AppCompatActivity {
 
-    //
+    // Categories list view
     String[] categories = {"Groceries", "Gas and Fuel", "Outgoing expenses", "Internet"};
     ListView listView;
+    Button addCat;
 
     // Frequencies Dropdown Menu
     Spinner spinner;
     String[] frequencies =  {"Daily","Weekly","Monthly","Customize"};
     ArrayAdapter<String> adapterItems;
+
+    // Other buttons
+    Button saveCh;
+    Button needHlp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +41,38 @@ public class Profile_v2 extends AppCompatActivity {
         adapterItems.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapterItems);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
-                String value = parent.getItemAtPosition(i).toString();
-                Toast.makeText(Profile_v2.this, value, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
         // Listview
         listView = (ListView) findViewById(R.id.simplelistview);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (this, R.layout.activity_simple_list_view, R.id.test, categories);
         listView.setAdapter(arrayAdapter);
+
+        // Adding a category when clicking the "Add Category Button"
+        addCat = (Button) findViewById((R.id.addCategory));
+        addCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("add cat", "onClick: HAHAHAHAHAH");
+            }
+        });
+
+        // Other buttons
+        saveCh = (Button) findViewById(R.id.saveButton2);
+        needHlp = (Button) findViewById(R.id.helpButton);
+
+        saveCh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Profile_v2.this, "Modifications saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        needHlp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("need help", "onClick: CHCHHCUCU");
+            }
+        });
+
 
     }
 }
