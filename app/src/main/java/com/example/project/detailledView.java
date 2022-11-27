a24ea516f8d4c67ba9a0445605a83076a645e6ff
AtinyStay <72931462+AtinyStay@users.noreply.github.com>
2022-11-27 10:44:20 +0000

    adding progress bars in detailed view

--- app/src/main/java/com/example/project/detailledView.java
+++ app/src/main/java/com/example/project/detailledView.java
@@ -2,15 +2,15 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.text.DecimalFormat;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class detailledView extends AppCompatActivity {
 
@@ -27,6 +27,13 @@ public class detailledView extends AppCompatActivity {
     public TextView gasPct;
     public TextView outgoingPct;
     public TextView netPct;
+
+    // Progress Bars
+    public ProgressBar grosPb;
+    public ProgressBar gasPb;
+    public ProgressBar outgoingPb;
+    public ProgressBar netPb;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +59,24 @@ public class detailledView extends AppCompatActivity {
         setPercentage(gasPct,gas,total_value);
         setPercentage(outgoingPct,outgoing,total_value);
         setPercentage(netPct,net,total_value);
+
+        // Progress bars
+        grosPb = (ProgressBar)findViewById(R.id.groceryPb);
+        gasPb = (ProgressBar)findViewById(R.id.carPb);
+        outgoingPb = (ProgressBar)findViewById(R.id.popcornPb);
+        netPb = (ProgressBar)findViewById(R.id.internetPb);
+
+        int grosVal = ((int) getPercentage(grocery, total_value));
+        int gasVal = ((int) getPercentage(gas, total_value));
+        int outgoingVal = ((int) getPercentage(outgoing, total_value));
+        int netVal = ((int) getPercentage(net, total_value));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            grosPb.setProgress(grosVal, true);
+            gasPb.setProgress(gasVal, true);
+            outgoingPb.setProgress(outgoingVal, true);
+            netPb.setProgress(netVal, true);
+        }
     }
 
     public double getTotal(double groc, double car, double popcorn, double internet) {
@@ -73,6 +98,22 @@ public class detailledView extends AppCompatActivity {
         String percentage = String.valueOf(round.format(getPercentage(txtview,total)));
         percentage += "%";
         pct.setText(percentage);
+    }
+
+    /*
+    public void setProgressBar(ProgressBar pb, int val) {
+        Timer t = new Timer();
+        TimerTask tt = new TimerTask() {
+            @Override
+            public void run()
+            {
+                counter++;
+                pb.setProgress(counter);
+                if(counter == val)
+                    t.cancel();
+            }
+        };
+        t.schedule(tt,0,50);
     }
 
     /*
