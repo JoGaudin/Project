663aafa8c1db3b6582fce5b4543a2b6a704d7898
AtinyStay <72931462+AtinyStay@users.noreply.github.com>
2022-11-27 10:43:51 +0000

    attempt to put a sound when add expense

--- app/src/main/java/com/example/project/addExpense_v2.java
+++ app/src/main/java/com/example/project/addExpense_v2.java
@@ -2,13 +2,38 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class addExpense_v2 extends AppCompatActivity {
+
+    public Button saveExp;
+    public MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense_v2);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+
+        // save expense leads to the sound effect
+        saveExp = (Button) findViewById(R.id.saveExpense);
+        saveExp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSound();
+            }
+        });
+    }
+
+    public void startSound() {
+        if(mediaPlayer != null) {
+            mediaPlayer.start();
+        }
     }
 }
